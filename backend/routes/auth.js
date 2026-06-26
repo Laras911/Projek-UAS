d6f0ecc0d7ea727b91a5b355d87a6998214d6a7d
Laras911 <larass9911@gmail.com>
2026-06-27 00:42:02 +0700

    telat

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const db = require('../database.js');
 const auth = require('../middleware/auth.js');
